054f72e61a0c1b0b132f20a9a03e73355351a799
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2017-07-24 16:53:11 +0100

    Moved the VTK view to its own component

---
+++ src/app/vtk.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
+import vtkActor from 'vtk.js/Sources/Rendering/Core/Actor';
+import vtkCalculator from 'vtk.js/Sources/Filters/General/Calculator';
+import vtkConeSource from 'vtk.js/Sources/Filters/Sources/ConeSource';
+import vtkMapper from 'vtk.js/Sources/Rendering/Core/Mapper';
+import { AttributeTypes } from 'vtk.js/Sources/Common/DataModel/DataSetAttributes/Constants';
+import { FieldDataTypes } from 'vtk.js/Sources/Common/DataModel/DataSet/Constants';
+
+
+import '../assets/css/styles.css';
+
+@Component({
+  selector: 'app-vtk',
+  templateUrl: './vtk.component.html',
+  styleUrls: ['./vtk.component.css']
+})
+export class VtkComponent implements OnInit {
+
+    @ViewChild('vtk') vtkRoot: ElementRef;
+
+    constructor() {}
+
+    public ngOnInit(): void {
+
+    // Set up VTK
+    const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
+      rootContainer: this.vtkRoot.nativeElement,
+      containerStyle: {}
+    });
+    const renderer = fullScreenRenderer.getRenderer();
+    const renderWindow = fullScreenRenderer.getRenderWindow();
+
+    const coneSource = vtkConeSource.newInstance({ height: 1.0 });
+    const filter = vtkCalculator.newInstance();
+    filter.setInputConnection(coneSource.getOutputPort());
+    filter.setFormula({
+      getArrays: (inputDataSets: any) => ({
+        input: [] as number[],
+        output: [
+          { location: FieldDataTypes.CELL, name: 'Random', dataType: 'Float32Array', attribute: AttributeTypes.SCALARS },
+        ],
+      }),
+      evaluate: (arraysIn: any, arraysOut: any) => {
+        const [scalars] = arraysOut.map((d: any) => d.getData());
+        for (let i = 0; i < scalars.length; i++) {
+          scalars[i] = Math.random();
+        }
+      },
+    });
+    const mapper = vtkMapper.newInstance();
+    mapper.setInputConnection(filter.getOutputPort());
+    const actor = vtkActor.newInstance();
+    actor.setMapper(mapper);
+    renderer.addActor(actor);
+    renderer.resetCamera();
+    renderWindow.render();
+    // -----------------------------------------------------------
+    // UI control handling
+    // -----------------------------------------------------------
+    // fullScreenRenderer.addController(controlPanel);
+    // const representationSelector = document.querySelector('.representations');
+    // const resolutionChange = document.querySelector('.resolution');
+    // representationSelector.addEventListener('change', (e) => {
+    //   const newRepValue = Number(e.target.value);
+    //   actor.getProperty().setRepresentation(newRepValue);
+    //   renderWindow.render();
+    // });
+    // resolutionChange.addEventListener('input', (e) => {
+    //   const resolution = Number(e.target.value);
+    //   coneSource.setResolution(resolution);
+    //   renderWindow.render();
+    // });
+    }
+}
