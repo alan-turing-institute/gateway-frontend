6a09b4180bab1de33e8626e90443e7e4db63735b
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2017-08-15 09:22:59 +0100

    slider restarts vtk object

--- src/app/vtk/pipe/vtk.component.ts
+++ src/app/vtk/pipe/vtk.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnChanges, OnDestroy, ViewChild, ElementRef, Input } from '@angular/core';
 
 import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
 
@@ -14,33 +14,35 @@ import vtkHttpDataSetReader       from 'vtk.js/Sources/IO/Core/HttpDataSetReader
 import { AttributeTypes } from 'vtk.js/Sources/Common/DataModel/DataSetAttributes/Constants';
 import { FieldDataTypes } from 'vtk.js/Sources/Common/DataModel/DataSet/Constants';
 
-
 @Component({
   selector: 'app-vtk',
   templateUrl: './vtk.component.html',
   styleUrls: ['./vtk.component.css']
 })
-export class VtkComponent implements OnInit {
+export class VtkComponent implements OnInit, OnChanges, OnDestroy {
 
     @ViewChild('vtk') vtkRoot: ElementRef;
+    // @Input() radius: number;
+    radius: number;
 
-    constructor() {}
+    constructor() {
+        this.radius = 5;
+    }
 
     public ngOnInit(): void {
-
+        console.log(" Init radius= ", this.radius)
         // Set up VTK
         const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
             rootContainer: this.vtkRoot.nativeElement,
             containerStyle: { }
         });
         fullScreenRenderer.getRenderWindow().getViews()[0].setSize([500,500]);
-                
         const renderer = fullScreenRenderer.getRenderer();
         const renderWindow = fullScreenRenderer.getRenderWindow();
         const actor = vtkActor.newInstance();
         const mapper = vtkMapper.newInstance();
 
-        const cylinderSource = vtkCylinderSource.newInstance({ height: 10.0, resolution: 50 });
+        const cylinderSource = vtkCylinderSource.newInstance({ height: 10.0, resolution: 50, radius: this.radius });
 
         actor.setMapper(mapper);
         mapper.setInputConnection(cylinderSource.getOutputPort());
@@ -48,5 +50,14 @@ export class VtkComponent implements OnInit {
         renderer.resetCamera();
         renderWindow.render();
 
+    }
+
+    public ngOnChanges(): void {
+        console.log("Changing radius to ...", this.radius);
+
+
+    }
+    public ngOnDestroy(): void {
+        console.log("Boom! 💣")
     }
 }
