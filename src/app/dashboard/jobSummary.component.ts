8e3f500fdc164f4c15442c4a669535d7342413a9
Radka Jersakova <r.jersakova@gmail.com>
2017-08-17 17:12:01 +0100

    resolved conflicts

--- src/app/dashboard/jobSummary.component.ts
+++ src/app/dashboard/jobSummary.component.ts
@@ -3,7 +3,7 @@ import { JobInfo } from './jobInfo';
 
 @Component({
   selector: 'jobSummary',
-<<<<<<< HEAD
+
   templateUrl: './jobSummary.component.html',
 //   template: `
 //     <div class="card card-job">
@@ -26,30 +26,7 @@ import { JobInfo } from './jobInfo';
 //             <p><small class="text-muted">Last updated 3 mins ago</small></p>
 //         </div>
 //     </div>`,
-=======
-  template: `
-    <div class="card card-job">
-        <div class="card-header" [ngClass]="getHeaderClass()">
-            <span class="badge" [ngClass]="getTitleClass()">
-                <i [ngClass]="getSpanIcon()"></i>
-            </span>
-            <strong>{{summary.status}}</strong>
-        </div>
-        <a routerLinkActive = "active" [routerLink] = "['/output/output', {id: summary.id}]">
-            <div class="wrapper">
-                <img class="card-img-top img-job" src="{{summary.thumbnail}}"
-                    (mouseover)='setJobHoverHidden()'
-                    (mouseleave)='setJobHoverHidden()'>
-                <i class="fa fa-sign-in fa-2x" [hidden]=jobHoverHidden></i>
-            </div>
-        </a>
-        <div class="card-block">
-            <p><a routerLinkActive = "active" [routerLink] = "['/output/output', {id: summary.id}]">
-              <button type="button" class="btn btn-primary">Details</button></a></p>
-            <p><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-    </div>`,
->>>>>>> ce46aec2f1a981a04dc78948b292ee36dc696e21
+
     styleUrls: ['jobSummary.css']
 })
 
@@ -76,7 +53,7 @@ export class JobSummaryComponent implements OnInit{
     getHeaderClass() : string {
         //   console.log("card-outline-"+this.summary.status.toLowerCase());
         return "card-header-"+this.summary.status.toLowerCase();
-<<<<<<< HEAD
+
     }
 
     getShortDescription(): string {
@@ -88,8 +65,7 @@ export class JobSummaryComponent implements OnInit{
             return false
         else
             return true
-=======
->>>>>>> ce46aec2f1a981a04dc78948b292ee36dc696e21
+
     }
 
     getSpanIcon() : string  {
@@ -116,9 +92,9 @@ export class JobSummaryComponent implements OnInit{
         if (this.summary.status == "Draft")
             icon ="fa fa-pencil-square fa-lg"
         return icon
-<<<<<<< HEAD
+
     }
-<<<<<<< HEAD
+
 
     getActionText() : string  {
         var text = ""
@@ -133,10 +109,3 @@ export class JobSummaryComponent implements OnInit{
         return text
     }
 }
-=======
-}
->>>>>>> output page tweaks
-=======
-    }
-}
->>>>>>> ce46aec2f1a981a04dc78948b292ee36dc696e21
