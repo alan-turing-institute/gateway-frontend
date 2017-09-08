e60acc934d8b94374fbe73ae0a4787ecbe19de20
masonlr <l.mason@imperial.ac.uk>
2017-09-08 19:02:30 +0100

    Fix trailing "/" as we're still not using a path.join library :(

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
     target: 'prod',
-    apiRoot: 'http://science-gateway-middleware-dev.azurewebsites.net',
+    apiRoot: 'http://science-gateway-middleware-dev.azurewebsites.net/',
     production: true
 }
