48864ff3431c522efea83d362821b5c422b2592c
masonlr <l.mason@imperial.ac.uk>
2017-09-10 20:01:55 +0100

    New angular video component

--- src/app/output/output.module.ts
+++ src/app/output/output.module.ts
@@ -2,21 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
-import {IonRangeSliderModule} from 'ng2-ion-range-slider';
+import { IonRangeSliderModule } from 'ng2-ion-range-slider';
 import { OutputComponent } from './output.component';
 import { OutputRoutingModule } from './output-routing.module';
 import { OutputService } from './output.service';
 import { PipeModule} from '../pipe.module';
 
-import {NgxChartsModule} from '@swimlane/ngx-charts';
+
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 //import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ChartsComponent } from './chart';
-import {ConfigModule} from '../config/config.module';
-import {DashboardModule} from '../dashboard/dashboard.module'
+import { ConfigModule } from '../config/config.module';
+import { DashboardModule } from '../dashboard/dashboard.module'
 
+import { VideoComponent } from './video/video.component';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-//import { DROPDOWN_DIRECTIVES } from '../shared/dropdown.directive';
 
 @NgModule({
   imports: [
@@ -31,10 +32,13 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
     NgxChartsModule,
     TabsModule.forRoot()
   ],
-  declarations: [OutputComponent,
-                  ChartsComponent//,
-//                DROPDOWN_DIRECTIVES
-              ],
-  providers : [OutputService]
+  declarations: [
+    OutputComponent,
+    ChartsComponent,
+    VideoComponent
+  ],
+  providers: [
+    OutputService
+  ]
 })
 export class OutputModule { }
---
+++ src/app/output/video/video.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-video',
+    templateUrl: './video.component.html',
+    styleUrls: ['./video.component.css']
+})
+
+export class VideoComponent implements OnInit {
+
+    @Input() videoUrl: string;
+
+    constructor() {
+        this.videoUrl = "";
+    }
+
+    public ngOnInit(): void {
+
+
+    }
+
+}
