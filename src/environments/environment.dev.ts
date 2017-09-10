7cd2dbdaf0d5cc465143d080b068ce1d2d5cc17d
masonlr <l.mason@imperial.ac.uk>
2017-09-10 13:30:58 +0100

    Hotfix api root url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,6 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
     target: 'dev',
-    apiRoot: 'http://localhost:8000/',
+    apiRoot: 'http://localhost:5000/api/',
     production: false
 }
