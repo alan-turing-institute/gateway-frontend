1b7a88a97659e3a54f4fdd04138310dd37321c24
Lachlan Mason <l.mason@imperial.ac.uk>
2017-11-07 10:39:39 +0000

    Remove clarity references

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import "../node_modules/clarity-icons/clarity-icons.min.css"
-import "../node_modules/@webcomponents/custom-elements/custom-elements.min.js"
-import "../node_modules/clarity-icons/clarity-icons.min.js"
-
-import "../node_modules/clarity-ui/clarity-ui.min.css"
 
 if (environment.production) {
   enableProdMode();
