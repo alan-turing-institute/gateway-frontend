410bc4f7ab5156c3d33ba49ea8e6e2905f5e4ad5
Lachlan Mason <l.mason@imperial.ac.uk>
2017-11-07 10:40:35 +0000

    Load clarity dependencies from vendor.ts file

--- src/vendor.ts
+++ src/vendor.ts
@@ -12,6 +12,15 @@ import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 
+import 'web-animations-js/web-animations.min';
+
+// Clarity Icons
+import 'clarity-icons/clarity-icons.min.css';
+import 'clarity-icons/clarity-icons.min';
+
+// Clarity UI
+import 'clarity-ui/clarity-ui.min.css';
+
 import 'ngx-bootstrap';
 import 'ngx-dropdown';
 import 'ng2-ion-range-slider';
