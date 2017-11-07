3deaab15c7dec252cc1f34c105a34168f5fe1f36
Lachlan Mason <l.mason@imperial.ac.uk>
2017-11-07 10:58:00 +0000

    Remove commented clarity imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,8 @@
 import { Component } from '@angular/core';
-// import {} from "clarity-icons/"
-
-// import "../assets/css/font-awesome.min.css"
-// import "../assets/css/simple-line-icons.css"
-// import '../scss/style.scss'
-
-// import "../../node_modules/clarity-icons/clarity-icons.min.css"
-// import "../../node_modules/@webcomponents/custom-elements/custom-elements.min.js"
-// import "../../node_modules/clarity-icons/clarity-icons.min.js"
-
-// import "../../node_modules/clarity-ui/clarity-ui.min.css"
-
-// import './layout/main.css'
 
 @Component({
     selector: 'body',
     template: '<router-outlet></router-outlet>',
-    //styleUrls: []
-    // styles:[require('../../node_modules/clarity-ui/clarity-ui.min.css').toString()]
 })
 export class AppComponent {
     constructor() {
--- src/app/layout/main.component.ts
+++ src/app/layout/main.component.ts
@@ -3,8 +3,7 @@ import { Component} from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.css']
-  // styles:[require('../../../node_modules/clarity-ui/clarity-ui.min.css').toString()]
+  styleUrls: ['./main.css'],
 })
 
 export class MainComponent {
