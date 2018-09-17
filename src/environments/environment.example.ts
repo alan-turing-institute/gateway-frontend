71685803d5684ca2038970a3522536a7324c6c9e
masonlr <l.mason@imperial.ac.uk>
2018-09-17 15:53:28 +0100

    feat: Disable token addition based on config

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,6 +1,7 @@
 export const environment = {
-  AUTH_URL: `http://localhost:5050`,
-  MIDDLEWARE_URL: `http://localhost:5000`,
+  AUTH_URL: 'http://localhost:5050',
+  MIDDLEWARE_URL: 'http://localhost:5000',
+  tokenBypass: ['core.windows.net'],
   production: true,
   useHash: false,
   hmr: false,
